3e45c9d2b9b429736e3205982c19a733e8ea221e
Moataz <moataz.wmm@gmail.com>
2025-05-18 21:43:44 +0300

    firebase integration.

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,17 @@ import 'package:evently_app_new/feature/onboarding/first_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/providers/theme_provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await ScreenUtil.ensureScreenSize();
 
   runApp(
